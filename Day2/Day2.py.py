436fe265a53d37e4d98486ac220fcb582fb6ad12
Hithul kannan <65595632+Hithul@users.noreply.github.com>
2020-11-25 01:25:58 +0530

    Add files via upload

---
+++ Day2/Day2.py.py
@@ -0,0 +1,18 @@
+#Exercise1
+a=b=c=50
+print(a/10)
+print(b*50)
+print(c+60)
+
+#Exercise2
+d= "robot"
+d = d.replace('b','t')
+print(d)
+
+#Wxercise3
+e = 2
+f = 2.5
+print(float(e))
+print(int(f))
+
+
